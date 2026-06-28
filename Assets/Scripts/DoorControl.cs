7b1399d4e330034b1a7158df41d2064516bc6ff2
Anthony Chan <achan@acm.org>
2026-06-28 17:39:25 -0400

    Implemented two door buttons

--- Assets/Scripts/DoorControl.cs
+++ Assets/Scripts/DoorControl.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using TMPro;
 
 public class DoorControl : MonoBehaviour
 {
     [SerializeField] private GameObject trigger;
     [SerializeField] private MeshRenderer doorRenderer;
+
+    [SerializeField] private TextMeshProUGUI lockStatusText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,12 +18,14 @@ public class DoorControl : MonoBehaviour
     {
         trigger.SetActive(true);
         doorRenderer.material.color = Color.green;
+        lockStatusText.text = "Unlocked";
     }
 
     public void LockDoor()
     {
         trigger.SetActive(false);
         doorRenderer.material.color = Color.red;
+        lockStatusText.text = "Locked";
     }
 
 }
